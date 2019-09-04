a866caec3aa2c5c25d561bf7057864e38e36a043
wight-airmash <38955076+wight-airmash@users.noreply.github.com>
2019-09-05 01:48:33 +0300

    Format

--- src/decoding/index.ts
+++ src/decoding/index.ts
@@ -1,22 +1,26 @@
-export const levelToBouty = (level: number): number => {
-    return ((level - 1) / 0.0111) ** 2;
+import { MAP_SIZE } from '../types/server';
+
+const clamp = (value: number, lower: number, upper: number): number => {
+  if (value <= lower) {
+    return lower;
+  }
+
+  if (value >= upper) {
+    return upper;
+  }
+
+  return value;
 };
 
-export function decodeMinimapCoords(x: number, y: number): { x: number, y: number } {
-    return {
-        x: 128 * x - 16384 + 64,
-        y: clamp(128 * y - 16384, -8192, 8192) + 64
-    };
-}
+export const levelToBouty = (level: number): number => {
+  return ((level - 1) / 0.0111) ** 2;
+};
 
-function clamp(value: number, lower: number, upper: number) {
-    if (value <= lower) {
-        return lower;
-    }
-    if (value >= upper) {
-        return upper;
-    }
-    return value;
-}
+export const decodeMinimapCoords = (x: number, y: number): { x: number; y: number } => {
+  return {
+    x: 128 * x - MAP_SIZE.WIDTH / 2 + 64,
+    y: clamp(128 * y - MAP_SIZE.HEIGHT, -MAP_SIZE.HEIGHT / 2, MAP_SIZE.HEIGHT / 2) + 64,
+  };
+};
 
 export default levelToBouty;
