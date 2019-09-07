e8f9ec67dba7fb5857a5c9b81997a501a877d77d
wight-airmash <38955076+wight-airmash@users.noreply.github.com>
2019-09-07 20:40:35 +0300

    Move types.

--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,9 @@
+import * as ClientPackets from './types/packets-client';
+import * as ServerPackets from './types/packets-server';
+
+export { ClientPackets, ServerPackets };
 export * from './support/errors';
+export * from './packets';
 export { default as CLIENT_PACKETS } from './packets/client';
 export { default as SERVER_PACKETS } from './packets/server';
 export { default as CLIENT_SCHEMAS } from './schemas/client';
